e8c4fa1a06ae86d856e907e69b90c52f5c35338d
Jakub Olan <jakub.jay.olan@gmail.com>
2021-01-13 22:41:54 +0100

    ci: add package building script

--- scripts/pack.js
+++ scripts/pack.js
@@ -1,5 +1,36 @@
-const jszip = require("jszip");
 const fs = require("fs");
+const path = require("path");
+const archiver = require("archiver");
 
-const modpackZip = new jszip();
-modpackZip.file("");
+var distDirectory = path.join(__dirname, "..", "dist");
+
+if (!fs.existsSync(distDirectory)) {
+  fs.mkdir(distDirectory, (e) => {
+    throw e;
+  });
+}
+
+var distArchive = path.join(__dirname, "..", "dist", "curseprofile.zip");
+
+var output = fs.createWriteStream(distArchive);
+var archive = archiver("zip");
+
+output.on("close", function () {
+  console.log(archive.pointer() + " bytes");
+});
+
+output.on("error", function (err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+var manifest = path.join(__dirname, "..", "manifest.json");
+var modlist = path.join(__dirname, "..", "modlist.html");
+var overrides = path.join(__dirname, "..", "overrides");
+
+archive
+  .append(fs.createReadStream(manifest), { name: "manifest.json" })
+  .append(fs.createReadStream(modlist), { name: "modlist.html" })
+  .directory(overrides, "overrides")
+  .finalize();
