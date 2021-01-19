f21809ed7673ac3e5c7e2cc57ee36da74cf6bc54
Jakub Olan <jakub.jay.olan@gmail.com>
2021-01-19 20:03:46 +0100

    chore: make function asynchronic (MA-266)

--- scripts/20-semanticRelease.js
+++ scripts/20-semanticRelease.js
@@ -7,25 +7,29 @@ const semanticReleaseConfiguration = require('../.releaserc.json')
 const stdoutBuffer = WriteableStreamBuffer()
 const stderrBuffer = WriteableStreamBuffer()
 
-try {
-	const result = await semanticRelease(semanticReleaseConfiguration, {
-		cwd: path.join(__dirname, '..'),
-		stdout: stdoutBuffer,
-		stderr: stderrBuffer,
-	})
+async function release() {
+	try {
+		const result = await semanticRelease(semanticReleaseConfiguration, {
+			cwd: path.join(__dirname, '..'),
+			stdout: stdoutBuffer,
+			stderr: stderrBuffer,
+		})
 
-	if (result) {
-		const { lastRelease, commits, nextRelease, releases } = result
-		console.log(`Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`)
-		if (lastRelease.version) console.log(`The last release was "${lastRelease.version}".`)
-		// TODO: Version should be bumped in package metafiles
-		for (const release of release) console.log(`The release was published with plugin "${release.pluginName}".`)
-	} else {
-		console.log('No release published.')
+		if (result) {
+			const { lastRelease, commits, nextRelease, releases } = result
+			console.log(`Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`)
+			if (lastRelease.version) console.log(`The last release was "${lastRelease.version}".`)
+			// TODO: Version should be bumped in package metafiles
+			for (const release of release) console.log(`The release was published with plugin "${release.pluginName}".`)
+		} else {
+			console.log('No release published.')
+		}
+
+		const logs = stdoutBuffer.getContentAsString('utf8')
+		const errors = stderrBuffer.getContentAsString('utf8')
+	} catch (err) {
+		console.error('The automated release failed with %0', err)
 	}
-
-	const logs = stdoutBuffer.getContentAsString('utf8')
-	const errors = stderrBuffer.getContentAsString('utf8')
-} catch (err) {
-	console.error('The automated release failed with %0', err)
 }
+
+release()
