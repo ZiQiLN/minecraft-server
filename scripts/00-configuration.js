fe5ea10a1ad14c4b50d1dd30ad409e109b9718e7
Jakub Olan <jakub.jay.olan@gmail.com>
2021-01-22 19:50:34 +0100

    ci: refactor continous delivery scripts

--- scripts/00-configuration.js
+++ scripts/00-configuration.js
@@ -8,12 +8,16 @@ const CF_TOKEN = process.env.CF_TOKEN
 const CF_API = 'minecraft.curseforge.com'
 
 // Directory Configuration
+const baseDirectory = path.join(__dirname, '..')
+const distDirectory = path.join(baseDirectory, 'dist')
 const modpackSourceDirectory = path.join(__dirname, '..', 'src')
 const serverSourceDirectory = path.join(__dirname, '..', 'serv')
 const modpackManifestPath = path.join(modpackSourceDirectory, 'manifest.json')
 const modpackOverridesDirectory = path.join(modpackSourceDirectory, 'overrides')
-const modpack
+const modpackBundleOutput = path.join(baseDirectory, 'dist', 'modpack.zip')
 
 module.exports = {
+	baseDirectory,
+	distDirectory,
 	modpackManifestPath,
 }
