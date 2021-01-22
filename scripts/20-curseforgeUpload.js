3a0fdc743291298f7ee49757c8f73f628ab5553f
Jakub Olan <jakub.jay.olan@gmail.com>
2021-01-22 21:23:04 +0100

    ci: makeshit cursefile-upload script

--- scripts/20-curseforgeUpload.js
+++ scripts/20-curseforgeUpload.js
@@ -1,19 +1,23 @@
 const got = require('got')
 const fs = require('fs')
 const path = require('path')
-const simplegit = require('simple-git')
-const git = simplegit(baseDirectory)
+// const simplegit = require('simple-git')
+// const git = simplegit(baseDirectory)
 const FormData = require('form-data')
 
 const { modpackManifestPath, baseDirectory, modpackBundleOutput, CF_TOKEN } = require('./00-configuration')
 const releaseNotes = require('../CHANGELOG.md')
 
-let lastestReleaseTag
+// git.checkIsRepo()
+// 	.then((isRepo) => !isRepo && initialiseRepo(git))
+// 	.then(() => git.fetch())
 
-git.pull().tags((err, tags) => {
-	console.log('Latest available tag: %s', tags.latest)
-	lastestReleaseTag = tags.latest
-})
+// let lastestReleaseTag
+
+// git.pull().tags((err, tags) => {
+// 	console.log('Latest available tag: %s', tags.latest)
+// 	lastestReleaseTag = tags.latest
+// })
 
 // TODO: Do not process script when there is no new release, since this code will create a new release without atention to actual releases.
 
@@ -22,7 +26,7 @@ const packageUploadForm = new FormData()
 form.append('changelog', releaseNotes)
 form.append('changelogType', 'markdown')
 form.append('gameVersions', [6756])
-form.append('displayName', `kubick-${lastestReleaseTag}`)
+form.append('displayName', `kubick`)
 form.append('releaseType', 'alpha')
 form.append('metafile', fs.createReadStream(modpackManifestPath))
 form.append('file', fs.createReadStream(modpackBundleOutput))
